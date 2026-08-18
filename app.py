4ffdc31ee6e9edcbe0612b8dd730bde72bc0c53b
vtryapi-cell <vtry.api@gmail.com>
2026-08-18 22:18:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -119,7 +119,3 @@ def test():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", "8000"))
     app.run(host="0.0.0.0", port=port)
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", "8000"))
-    app.run(host="0.0.0.0", port=port)
